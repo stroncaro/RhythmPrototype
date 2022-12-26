71ab04faa9688ed834c8cb56e80858240a10ead1
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-12-26 15:58:48 -0300

    Squashed commit of the following:
    commit dea282be32530daadba8b58f10451d90fb06cc71
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Mon Dec 26 15:36:38 2022 -0300
    
        create Event system
    
    commit b6f6bceb0c24e21d72c5a96ba8244d0abc832126
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Mon Dec 26 04:16:47 2022 -0300
    
        add OnBeatPlayed event for IMetronome
    
    commit 28518fc4a23752b5aaa7f8580dbcd745b42f3005
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Mon Dec 26 02:58:56 2022 -0300
    
        implement Metronome_procedural
    
    commit a4b127271e552bafa663b6ed6440ccbf51dc23b3
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Mon Dec 26 00:03:46 2022 -0300
    
        create Audio assembly definition
    
    commit c172358125ac05adfc8b929651e532ad184be59e
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Mon Dec 26 00:03:33 2022 -0300
    
        extract interfaces to their own files
    
        created IMetronome.cs and ISoundManager.cs
    
    commit 722ede6710a644656b3104aa0a8524a54b974030
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Sun Dec 25 23:59:51 2022 -0300
    
        create empty ISoundManager interface
    
    commit b468ca13f2b3c08bf3364e13fe025af3b53a58e5
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Sun Dec 25 23:55:52 2022 -0300
    
        add Metronome GObj to Sound Manager Tests scene
    
    commit 6014d9edf9b17642afa8f1ebe599464256e196f9
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Sun Dec 25 23:53:02 2022 -0300
    
        create empty Metronome_procedural class and IMetronome interface
    
    commit a1b5eb6eb58e875128964fdaadea5e00ef82e6ae
    Author: Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
    Date:   Sun Dec 25 23:46:47 2022 -0300
    
        create SoundManager singleton class

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour, ISoundManager
+{
+    public static SoundManager Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("Attempting to create a SoundManager, but one already exists");
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
