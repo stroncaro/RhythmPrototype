cff8bf98c9499ce6821d1813f2efebcc2947ff64
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-12-26 17:11:57 -0300

    move delayed event management to EventManager_delayedEvents

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -1,23 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 public partial class EventManager : SingletonMonoBehaviour<EventManager>
 {
-    private struct DelayedEvent
-    {
-        public Action<object, EventArgs> trigger;
-        public object sender;
-        public EventArgs args;
-    }
-
-    private Queue<DelayedEvent> DelayedEvents = new Queue<DelayedEvent>();
-
-    private void Update()
-    {
-        while (DelayedEvents.Count > 0)
-        {
-            DelayedEvent e = DelayedEvents.Dequeue();
-            e.trigger.Invoke(e.sender, e.args);
-        }
-    }
 }
